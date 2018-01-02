bb90023724d7fa74fbe57752ff502d953a49d8ce
Sam Song <phpman.song@gmail.com>
2018-01-02 18:06:14 +0800

    Create install_vscode_exts.sh
    Quick to set up VS Code extensions on a new MAC.

---
+++ install_vscode_exts.sh
@@ -0,0 +1,36 @@
+#!/bin/bash 
+# Windows %USERPROFILE%\.vscode\extensions
+# Mac ~/.vscode/extensions
+# Linux ~/.vscode/extensions
+
+cd ${HOME}/.vscode/extensions
+vscode_ets=(
+  Equinusocio.vsc-material-theme
+  PKief.material-icon-theme
+  akamud.vscode-theme-onedark
+  alefragnani.project-manager
+  bmewburn.vscode-intelephense-client
+  calebporzio.better-phpunit
+  dbaeumer.vscode-eslint
+  eamodio.gitlens
+  felixfbecker.php-debug
+  formulahendry.auto-close-tag
+  ikappas.composer
+  ikappas.phpcs
+  junstyle.php-cs-fixer
+  lukehoban.Go
+  nikitaKunevich.snippet-creator
+  octref.vetur
+  patbenatar.advanced-new-file
+  ryannaddy.laravel-artisan
+  sldobri.daily
+  sleistner.vscode-fileutils
+  vscodevim.vim
+  wix.vscode-import-cost
+)
+
+for et_name in ${vscode_ets[@]};  
+do  
+    code --install-extension $et_name
+    echo $et_name 'done'
+done 
